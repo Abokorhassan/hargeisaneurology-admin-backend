c5cd62817cb5c953cc133e14d5eff708a87c7fa6
abokorhassan <abokor13@hotmail.com>
2019-12-01 12:09:48 +0300

    populating "first_name' and 'second_name' so database accept it

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'second_name', 'email', 'password',
     ];
 
     /**
